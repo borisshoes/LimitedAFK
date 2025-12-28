ce84082398d6a5910fbb88a4395f9da28367439a
borisshoes <boris@borisshoes.net>
2025-12-28 13:07:30 -0500

    Added server-sided only storage code checks for Essential Client hosting compatibility.

--- src/main/java/net/borisshoes/limitedafk/callbacks/InteractionsCallback.java
+++ src/main/java/net/borisshoes/limitedafk/callbacks/InteractionsCallback.java
@@ -2,6 +2,8 @@ package net.borisshoes.limitedafk.callbacks;
 
 import net.borisshoes.borislib.datastorage.DataAccess;
 import net.borisshoes.limitedafk.PlayerData;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
@@ -13,21 +15,25 @@ import org.jetbrains.annotations.Nullable;
 
 public class InteractionsCallback {
    public static InteractionResult useItem(Player player, Level world, InteractionHand hand){
+      if(!(player instanceof ServerPlayer serverPlayer) || !(world instanceof ServerLevel)) return InteractionResult.PASS;
       DataAccess.getPlayer(player.getUUID(),PlayerData.KEY).updateActionTime("playerInteract",System.currentTimeMillis());
       return InteractionResult.PASS;
    }
    
    public static InteractionResult useEntity(Player player, Level world, InteractionHand hand, Entity entity, @Nullable EntityHitResult entityHitResult){
+      if(!(player instanceof ServerPlayer serverPlayer) || !(world instanceof ServerLevel)) return InteractionResult.PASS;
       DataAccess.getPlayer(player.getUUID(),PlayerData.KEY).updateActionTime("playerInteract",System.currentTimeMillis());
       return InteractionResult.PASS;
    }
    
    public static InteractionResult useBlock(Player player, Level world, InteractionHand hand, BlockHitResult blockHitResult){
+      if(!(player instanceof ServerPlayer serverPlayer) || !(world instanceof ServerLevel)) return InteractionResult.PASS;
       DataAccess.getPlayer(player.getUUID(),PlayerData.KEY).updateActionTime("playerInteract",System.currentTimeMillis());
       return InteractionResult.PASS;
    }
    
    public static InteractionResult attackEntity(Player player, Level world, InteractionHand hand, Entity entity, @Nullable EntityHitResult entityHitResult){
+      if(!(player instanceof ServerPlayer serverPlayer) || !(world instanceof ServerLevel)) return InteractionResult.PASS;
       DataAccess.getPlayer(player.getUUID(),PlayerData.KEY).updateActionTime("playerInteract",System.currentTimeMillis());
       return InteractionResult.PASS;
    }
